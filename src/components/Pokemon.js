f2bb616f4c9d2dbac2511f18491b53eb5f62cccd
elosis <96085941+elosis@users.noreply.github.com>
2023-04-16 23:45:05 +0300

    removing render test from Pokemon

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../App.css";
 import { DexContext, useContext } from "../store/context";
-import { render } from "@testing-library/react";
 
 export default function Pokemon() {
   const { renderPokemon, pokemonData } = useContext(DexContext);
