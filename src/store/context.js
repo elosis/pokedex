a6349bb5f1390ebff3d657feda83f146bb94853e
elosis <96085941+elosis@users.noreply.github.com>
2023-04-21 22:14:58 +0300

    f 3

--- src/store/context.js
+++ src/store/context.js
@@ -193,9 +193,9 @@ export default function DexProvider(props) {
         </div>
         <div
           className="details-button"
-          style={{
-            marginTop: pokemonData.weaknesses.length > 5 ? "20px" : "10px",
-          }}
+          // style={{
+          //   marginTop: pokemonData.weaknesses.length > 5 ? "20px" : "10px",
+          // }}
         >
           <span onClick={getPrevPokemon}>&#60;</span>
           <div>#0{pokemonID}</div>
