4c6c52f5f998bbc120dcea7643bd9524695af4e5
elosis <96085941+elosis@users.noreply.github.com>
2023-05-11 01:45:22 +0300

    fixing Evo

--- src/components/EvolutionChain.js
+++ src/components/EvolutionChain.js
@@ -2,26 +2,103 @@ import React from "react";
 import "../App.css";
 import { DexContext, useContext } from "../store/context";
 
-export default function EvoChain({ id }) {
-  const { renderEvolution, evolutionData, getNextPokemon, getPrevPokemon } =
+export default function EvolutionChain() {
+  const { getPrevPokemon, getNextPokemon, pokemonID, evolutionData } =
     useContext(DexContext);
 
-  return (
-    <div className="evolution-chain">
-      <div className="evo-header">
-        <div>Evolution Chain</div>
-        <div>
-          <img src="tree.png"></img>
+  if (!evolutionData) {
+    return null;
+  }
+
+  // Check if there are two evolutions in the chain
+  const hasTwoEvolutions =
+    evolutionData.evolves_to.length === 1 &&
+    evolutionData.evolves_to[0].evolves_to.length === 1;
+
+  if (hasTwoEvolutions) {
+    const firstEvolution = evolutionData.evolves_to[0];
+    const secondEvolution = firstEvolution.evolves_to[0];
+
+    return (
+      <div className="evolution-chain">
+        <div className="evo-header">
+          <div>Evolution Chain</div>
+          <div>
+            <img src="tree.png"></img>
+          </div>
+        </div>
+        <div className="evolution-container">
+          <div className="evolution-item">
+            <div>
+              <p>{evolutionData.species.name}</p>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                  evolutionData.species.url.split("/").slice(-2)[0]
+                }.png`}
+                alt={evolutionData.species.name}
+                style={{ width: 120, height: 120 }}
+              />
+            </div>
+          </div>
+          <div className="evolution-item">
+            <div>
+              <p>{firstEvolution.species.name}</p>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                  firstEvolution.species.url.split("/").slice(-2)[0]
+                }.png`}
+                alt={firstEvolution.species.name}
+                style={{ width: 120, height: 120 }}
+              />
+            </div>
+          </div>
+          <div className="evolution-item">
+            <div>
+              <p>{secondEvolution.species.name}</p>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                  secondEvolution.species.url.split("/").slice(-2)[0]
+                }.png`}
+                alt={secondEvolution.species.name}
+                style={{ width: 120, height: 120 }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="evo-button">
+          <span onClick={getPrevPokemon}>&#60;</span>
+          <div className="button-name">#0{pokemonID}</div>
+          <span onClick={getNextPokemon}>&#62;</span>
         </div>
       </div>
-      <div className="evo-pics-container">
-        {evolutionData && renderEvolution(evolutionData)}
-      </div>
-      <div className="evo-button">
-        <span onClick={getPrevPokemon}>&#60;</span>
-        <div className="button-name">#0{id}</div>
-        <span onClick={getNextPokemon}>&#62;</span>
-      </div>
+    );
+  }
+
+  return (
+    <div>
+      {evolutionData.evolves_to.length > 0 && (
+        <div>
+          <div className="evo-pics-container">
+            <div className="evolution-container">
+              {evolutionData.evolves_to.map((evolution) => (
+                <div key={evolution.species.name} className="evolution-item">
+                  <div>
+                    <p>{evolution.species.name}</p>
+                    <img
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                        evolution.species.url.split("/").slice(-2)[0]
+                      }.png`}
+                      alt={evolution.species.name}
+                      style={{ width: 120, height: 120 }}
+                    />
+                  </div>
+                  {EvolutionChain(evolution)}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/store/context.js
+++ src/store/context.js
@@ -64,87 +64,6 @@ export default function DexProvider(props) {
     setEvolutionData(evolutionChainData.chain);
   };
 
-  const renderEvolution = (evolutionData) => {
-    if (!evolutionData) {
-      return null;
-    }
-
-    // Check if there are two evolutions in the chain
-    const hasTwoEvolutions =
-      evolutionData.evolves_to.length === 1 &&
-      evolutionData.evolves_to[0].evolves_to.length === 1;
-
-    if (hasTwoEvolutions) {
-      const firstEvolution = evolutionData.evolves_to[0];
-      const secondEvolution = firstEvolution.evolves_to[0];
-
-      return (
-        <div className="evolution-container">
-          <div className="evolution-item">
-            <div>
-              <p>{evolutionData.species.name}</p>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                  evolutionData.species.url.split("/").slice(-2)[0]
-                }.png`}
-                alt={evolutionData.species.name}
-                style={{ width: 120, height: 120 }}
-              />
-            </div>
-          </div>
-          <div className="evolution-item">
-            <div>
-              <p>{firstEvolution.species.name}</p>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                  firstEvolution.species.url.split("/").slice(-2)[0]
-                }.png`}
-                alt={firstEvolution.species.name}
-                style={{ width: 120, height: 120 }}
-              />
-            </div>
-          </div>
-          <div className="evolution-item">
-            <div>
-              <p>{secondEvolution.species.name}</p>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                  secondEvolution.species.url.split("/").slice(-2)[0]
-                }.png`}
-                alt={secondEvolution.species.name}
-                style={{ width: 120, height: 120 }}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        {evolutionData.evolves_to.length > 0 && (
-          <div className="evolution-container">
-            {evolutionData.evolves_to.map((evolution) => (
-              <div key={evolution.species.name} className="evolution-item">
-                <div>
-                  <p>{evolution.species.name}</p>
-                  <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                      evolution.species.url.split("/").slice(-2)[0]
-                    }.png`}
-                    alt={evolution.species.name}
-                    style={{ width: 120, height: 120 }}
-                  />
-                </div>
-                {renderEvolution(evolution)}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  };
-
   useEffect(() => {
     getPoke();
   }, [pokemonID]);
@@ -161,7 +80,6 @@ export default function DexProvider(props) {
     getPoke,
     getNextPokemon,
     getPrevPokemon,
-    renderEvolution,
   };
 
   return (
